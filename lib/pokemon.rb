c4bb49409a32db682af5f9d839a8da6b5a041d3a
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-10-08 00:13:28 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
+require 'pry'
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name 
+    @type = type 
+    @db = db
+  end
+  
+  def self.save(name, type)
+    #binding.pry 
+    
+    sql =
+    "INSERT INTO pokemon (name,type)
+    VALUE (?, ?)"
+    
+    DB[:conn].execute(sql, self.name. self.type)
+    id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end 
 end
